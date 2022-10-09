b26391e8dd799615562d2069442e47e9b62cb1e0
Syed00711 <syed.muzammils@gmail.com>
2022-10-09 17:35:44 +0530

    new

---
+++ selenium_testng/src/main/java/com/microcare/selenium/App.java
@@ -0,0 +1,13 @@
+package com.microcare.selenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
